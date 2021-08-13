dc9a46a84b0c262b8f3845a27f8d0ac731e8de6e
ttyyaaa <elpeke0306@gmail.com>
2021-08-13 17:06:03 +0900

    最初のコミット

---
+++ app/src/main/java/jp/techacademy/toya/kobayashi/MainActivity.kt
@@ -0,0 +1,11 @@
+package jp.techacademy.toya.kobayashi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
